85f44ef8de57d4ab1f5a7c5d4139ccfa58108d97
Glenn Bech <glenn.bech@gmail.com>
2018-10-29 00:03:36 +0100

    statuscake

--- concourse/terraform/terraform.sh
+++ concourse/terraform/terraform.sh
@@ -21,6 +21,7 @@ setup() {
     export DIR="$PWD"
     export GITHUB_TOKEN="${github_token}"
     export HEROKU_API_KEY="${heroku_api_key}"
+    export STATUSCAKE_APIKEY="${statuscake_api_key}"
 }
 
 setup_cache() {
